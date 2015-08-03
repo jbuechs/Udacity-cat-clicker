9d78fb8fed7f395514cb3d2598d3c35393bdd53c
jbuechs <janina.buechner@gmail.com>
2015-08-03 11:39:42 -0400

    Refactor to vanilla javascript to see if I could do it. Success!

--- js/app.js
+++ js/app.js
@@ -1,117 +1,117 @@
-$(function(){
-	// Model
-	var catsModel = {
-		'currentCat' : null,
-		'cat' : [
-			{
-				'name' : 'Fido',
-				'url' : 'img/cat1.jpg',
-				'score' : 0,
-			},
-			{
-				'name' : 'Frank',
-				'url' : 'img/cat2.jpg',
-				'score' : 0,
-			},
-			{
-				'name' : 'Steven',
-				'url' : 'img/cat3.jpg',
-				'score' : 0,
-			},
-			{
-				'name' : 'Eloise',
-				'url' : 'img/cat4.jpg',
-				'score' : 0,
-			},
-			{
-				'name' : 'Ugly',
-				'url' : 'img/cat5.jpg',
-				'score' : 0,
-			},
-			{
-				'name' : 'Hortensia',
-				'url' : 'img/cat6.jpg',
-				'score' : 0,
-			},
-			{
-				'name' : 'Kitty',
-				'url' : 'img/cat7.jpg',
-				'score' : 0,
-			},
-		],
-	};
+// Model
+var catsModel = {
+	'currentCat' : null,
+	'cat' : [
+		{
+			'name' : 'Fido',
+			'url' : 'img/cat1.jpg',
+			'score' : 0,
+		},
+		{
+			'name' : 'Frank',
+			'url' : 'img/cat2.jpg',
+			'score' : 0,
+		},
+		{
+			'name' : 'Grumpy Cat',
+			'url' : 'img/cat3.jpg',
+			'score' : 0,
+		},
+		{
+			'name' : 'Eloise',
+			'url' : 'img/cat4.jpg',
+			'score' : 0,
+		},
+		{
+			'name' : 'Hortensia',
+			'url' : 'img/cat5.jpg',
+			'score' : 0,
+		},
+		{
+			'name' : 'Breadface',
+			'url' : 'img/cat6.jpg',
+			'score' : 0,
+		},
+		{
+			'name' : 'Kitty',
+			'url' : 'img/cat7.jpg',
+			'score' : 0,
+		},
+	],
+};
 
-	// Octopus
-	var octopus = {
-		init: function() {
-			catsModel.currentCat = catsModel.cat[0];
-			catView.init();
-			catListView.init();
+// Octopus
+var octopus = {
+	init: function() {
+		catsModel.currentCat = catsModel.cat[0];
+		catView.init();
+		catListView.init();
 
-		},
-		getCurrentCat: function() {
-			return catsModel.currentCat;
-		},
-		getCats: function() {
-			return catsModel.cat;
-		},
-		setCurrentCat: function(cat) {
-			catsModel.currentCat = cat;
-		},
-		increment_score: function() {
-			catsModel.currentCat.score++;
-			catView.render();
+	},
+	getCurrentCat: function() {
+		return catsModel.currentCat;
+	},
+	getCats: function() {
+		return catsModel.cat;
+	},
+	setCurrentCat: function(cat) {
+		catsModel.currentCat = cat;
+	},
+	increment_score: function() {
+		catsModel.currentCat.score++;
+		catView.render();
+	}
+
+};
+
+// Cat Detail View
+var catView = {
+	init: function() {
+		//Store pointers to the DOM elements for easy access later
+		this.catName = document.getElementById('catName');
+		console.log(this.catName);
+		this.catImage = document.getElementById('catImage');
+		this.catScore = document.getElementById('catScore');
+		catImage.addEventListener('click', function(){
+			octopus.increment_score();
+		});
+		this.render();
+	},
+	render: function() {
+		var catObject = octopus.getCurrentCat();
+		this.catName.innerHTML = catObject.name;
+		this.catImage.src = catObject.url;
+		this.catScore.innerHTML = ('Number of clicks: ' + catObject.score);
+	}
+};
+
+// Cat List View
+var catListView = {
+	init: function() {
+		this.render();
+	},
+	render: function() {
+		var cat, elem, i;
+		var catArray = octopus.getCats();
+		for (i = 0; i < catArray.length; i++) {
+			cat = catArray[i]; // The current cat we're looping over
+			elem = document.createElement('li');
+			elem.textContent = cat.name;
+
+			// on click, setCurrentCat and render the catView
+			// this uses a closure-in-a-loop trick to connect the value
+			// of the cat variable to the click event function
+			elem.addEventListener('click', (function(catCopy) {
+				return function() {
+					octopus.setCurrentCat(catCopy);
+					catView.render();
+				};
+			})(cat));
+
+			// add the element to the list
+			document.getElementById('catlist').appendChild(elem);
 		}
+	}
+};
 
-	};
-
-	// Cat Detail View
-	var catView = {
-		init: function() {
-			this.catName = $('#catName');
-			this.catImage = $('#catImage');
-			this.catScore = $('#catScore');
-			this.catImage.click(function(){
-				octopus.increment_score();
-			});
-			this.render();
-		},
-		render: function() {
-			var catObject = octopus.getCurrentCat();
-			this.catName.text(catObject.name);
-			this.catImage.attr('src', catObject.url);
-			this.catScore.text('Number of clicks: ' + catObject.score);
-		}
-	};
-
-	// Cat List View
-	var catListView = {
-		init: function() {
-			this.render();
-		},
-		render: function() {
-			var cat, elem, i;
-			var catArray = octopus.getCats();
-			for (i = 0; i < catArray.length; i++) {
-				cat = catArray[i]; // The current cat we're looping over
-				elem = document.createElement('li');
-				elem.textContent = cat.name;
-
-				// on click, setCurrentCat and render the catView
-				// this uses a closure-in-a-loop trick to connect the value
-				// of the cat variable to the click event function
-				elem.addEventListener('click', (function(catCopy) {
-					return function() {
-						octopus.setCurrentCat(catCopy);
-						catView.render();
-					};
-				})(cat));
-
-				// add the element to the list
-				$('#catlist').append(elem);
-			}
-		}
-	};
-
-	octopus.init();
-});
+octopus.init();
